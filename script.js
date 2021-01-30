033b1f5f610f944a4be72113532bfae5925d5f92
Alex Dominguez Garcia <alexand.dominguez@gmail.com>
2021-01-30 16:30:50 -0500

    Commit.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // =====================================
 // PlayerFactory
 // =====================================
-const PlayerFactory = (marker) => {
-  return { marker };
+const PlayerFactory = (name) => {
+  return { name };
 };
 
 // =====================================
@@ -21,9 +21,14 @@ const GameBoard = (() => {
     Game.calculateWinner();
   };
 
+  const resetBoard = () => {
+    board.fill('');
+  };
+
   return {
-    board,
+    resetBoard,
     updateBoard,
+    board
   };
 })();
 
@@ -31,27 +36,54 @@ const GameBoard = (() => {
 // Game
 // =====================================
 const Game = (() => {
-  const _players = {
-    playerOne: PlayerFactory("X"),
-    playerTwo: PlayerFactory("O"),
+  const _players = [];
+  let _currentMove = null;
+
+  const startGame = (...names) => {
+    let marker = "X";
+
+    names.forEach((name, i) => {
+      _players.push( PlayerFactory(name) );
+      _players[i].marker = marker;
+      marker = (marker === "X") ? "O" : "X";
+    });
+
+    _currentMove = _players[0];
   };
-  let _currentMove = _players.playerOne.marker;
 
-  const _setMove = (targetCell) => {
-    GameBoard.updateBoard(targetCell.id, _currentMove);
-    _currentMove = (_currentMove === _players.playerOne.marker) ?
-        _players.playerTwo.marker : _players.playerOne.marker;
-  };
+  const validateMove = (event) => {
+    const targetSquare = event.target;
 
-  const isValidMove = (event) => {
-    const targetCell = event.target;
-
-    if (targetCell.textContent !== '') {
+    if (!_isEmptySquare(targetSquare)) {
       return;
     }
 
-    _setMove(targetCell);
+    _setMove(targetSquare);
+  }
+
+  const _isEmptySquare = (targetCell) => {
+    if (targetCell.textContent !== '') {
+      return false;
+    }
+
+    return true;
   };
+
+  const _setMove = (targetCell) => {
+    GameBoard.updateBoard(targetCell.id, _currentMove.marker);
+    _currentMove = (_currentMove === _players[0]) ? _players[1] :
+        _players[0];
+  };
+
+  const resetGame = () => {
+    GameBoard.resetBoard();
+    DisplayController.resetDisplay();
+  };
+
+  const playAgain = () => {
+    GameBoard.resetBoard();
+    DisplayController.replay();
+  }
 
   const calculateWinner = () => {
     const winningLines = [
@@ -89,9 +121,12 @@ const Game = (() => {
   };
 
   return {
-    isValidMove,
+    validateMove,
     declareWinner,
-    calculateWinner
+    calculateWinner,
+    resetGame,
+    startGame,
+    playAgain
   };
 })();
 
@@ -99,33 +134,105 @@ const Game = (() => {
 // DisplayController
 // =====================================
 const DisplayController = (() => {
+  const _scoreboardInput = document.querySelector(".scoreboard-input");
+  const _scoreboardActive = document.querySelector(".scoreboard-active");
+  const _scoreBoardSelect = document.querySelector(".scoreboard-playerSelect");
+  const _playerOneName = document.querySelector(".playerOneName");
+  const _playerTwoName = document.querySelector(".playerTwoName");
+  const _playerOneInput = document.querySelector("#playerOne");
+  const _playerTwoInput = document.querySelector("#playerTwo");
   const _table = Array.from( document.querySelectorAll("td") );
 
-  // TODO
-  const _init = () => {
-    GameBoard.board.fill('');
-    document.querySelector(".modal-hiding").classList.remove('modal-showing');
+  const _bindEvents = () => {
+    document.querySelector("input[value='human']")
+        .addEventListener("change", _toggleInputRow);
+    document.querySelector("input[value='computer']")
+        .addEventListener("change", _startGame);
+    document.querySelector(".scoreboard-input .button")
+        .addEventListener("click", _startGame);
+    document.querySelector(".scoreboard-active .button")
+        .addEventListener("click", Game.resetGame);
+    document.querySelector(".modal-modalBody .button")
+        .addEventListener("click", Game.playAgain);
+  };
+
+  const _startGame = (event) => {
+    if (event.target.value === "computer") {
+      _playerOneName.textContent = "Human";
+      _playerTwoName.textContent = "Computer";
+
+      Game.startGame("Human", "Computer");
+      _togglePlayerSelect();
+    } else {
+      const playerOne  = (_playerOneInput.value) ? _playerOneInput.value :
+          _playerOneInput.placeholder;
+      const playerTwo = (_playerTwoInput.value) ? _playerTwoInput.value :
+          _playerTwoInput.placeholder;
+
+      Game.startGame(playerOne, playerTwo);
+      _playerOneName.textContent = playerOne;
+      _playerTwoName.textContent = playerTwo;
+      _toggleInputRow();
+    }
+
+    _togglePlayerSelect();
+    _toggleActiveRow();
     renderBoard();
+  };
+
+  const _toggleInputRow = () => {
+    _scoreboardInput.classList.toggle("scoreboard-input-hiding");
+  };
+
+  const _toggleActiveRow = () => {
+    _scoreboardActive.classList.toggle("scoreboard-active-hiding");
+  };
+
+  const _togglePlayerSelect = () => {
+    _scoreBoardSelect.classList.toggle("scoreboard-playerSelect-hiding");
+  };
+
+  const init = () => {
+    _bindEvents();
   };
 
   const renderBoard = () => {
     _table.forEach((cell, i) => {
       cell.id = i;
       cell.textContent = GameBoard.board[i];
-      cell.addEventListener('click', Game.isValidMove);
+      cell.addEventListener('click', Game.validateMove);
     });
   };
 
   const renderModal = (text) => {
     document.querySelector(".modal-hiding").classList.add('modal-showing');
     document.querySelector(".modal-modalHeaderTitle").textContent = text;
-    document.querySelector('.button').addEventListener("click", _init);
+  };
+
+  const _clearInputFields = () => {
+    _playerOneInput.value = '';
+    _playerTwoInput.value = '';
+  };
+
+  const resetDisplay = () => {
+    _clearInputFields();
+    _toggleActiveRow();
+    _toggleInputRow();
+    renderBoard();
+  };
+
+  const replay = () => {
+    document.querySelector(".modal-hiding").classList.remove('modal-showing');
+    renderBoard();
   };
 
   return {
     renderBoard,
     renderModal,
+    resetDisplay,
+    replay,
+    init
   };
 })();
 
-DisplayController.renderBoard();
+DisplayController.init();
